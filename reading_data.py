d5adf4b269f1f773f34bd38e14f68b70e73d0685
Sklodowska <anna.sklodowska@bam.de>
2023-06-01 18:16:28 +0200

    extracted data to DF and added seaborn plot

--- reading_data.py
+++ reading_data.py
@@ -13,6 +13,8 @@ import vallenae as vae
 import os
 import fnmatch
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 #%% importing data
 
@@ -59,3 +61,76 @@ ax.plot(waveform[1], waveform[0])
 ax.set_xlabel("time [ms]")
 ax.set_ylabel("amplitude [mV]")
 ax.set_title(f"waveform hit #trai {trai}")
+
+
+
+#%% extracting labels and TRAIs
+
+
+kk = pd.read_excel(r'C:\Users\asklodow\Desktop\AE\Sensor_callibration\20230526_sensor_callibration_test/PLB_test.xlsx', index_col= None )
+
+headers = kk.columns.values
+
+ids = headers[0]
+trais = headers[-1]
+label = headers[1]
+
+trais_dict = {}
+keys = []
+
+rows = 0
+while rows < len(kk)-1:
+    if kk.iloc[rows][ids] == "La" and kk.iloc[rows+1][ids] == "Ht":
+        name = kk.iloc[rows][label].split(" ")      
+        key = name[-1]
+        keys.append(key)
+        values = []
+        rows += 1
+    
+    if kk.iloc[rows][ids] == "Ht":
+        values.append(kk.iloc[rows][trais])
+        trais_dict[f'{key}'] = values
+        
+    rows += 1
+        
+#%% redefining the dictionary
+data_dict = {}
+
+pp = 0
+for keys, values in trais_dict.items():
+    sensor_angle = keys.split("_")
+    angle = sensor_angle[1] 
+    if pp == 0:
+        sensor = sensor_angle[0]       
+        data_dict[f"{sensor}"] = {angle: values} 
+        pp = 1
+    else:     
+        if sensor == sensor_angle[0]:
+            data_dict[f"{sensor}"][f'{angle}'] = values
+        else:
+            sensor = sensor_angle[0]
+            data_dict[f"{sensor}"] = {angle: values} 
+
+
+#%% Creating the dataframe
+lol = []
+for sensors, angles in data_dict.items():
+    for angle, values in angles.items():
+        print(sensors, angle, values)
+        for value in values:
+            amplitude = hit_df[hit_df["trai"] == value]
+            ampl = amplitude.loc[amplitude.index[0]]["amplitude"]
+            lol.append([sensors, angle, ampl, value])
+new_df = pd.DataFrame(lol, columns = ['Sensor', 'Angle', 'Amplitude', 'Trai'])
+
+# plotting the data
+fig, ax = plt.subplots(figsize = (15,5))
+sns.boxplot(new_df, x = 'Angle', y = 'Amplitude', hue = 'Sensor')
+
+
+
+
+
+
+
+
